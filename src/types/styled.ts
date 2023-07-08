e1589d49366fe8a8d68c84957f757067d47aa8eb
MKAbuMattar <mohammad.khaled@outlook.com>
2023-07-08 13:45:24 +0300

    changeset: dependencie prettier@2.8.8

--- src/types/styled.ts
+++ src/types/styled.ts
@@ -1,12 +1,22 @@
-import type { AnchorHTMLAttributes, DetailedHTMLProps , ImgHTMLAttributes } from 'react';
+import type {
+  AnchorHTMLAttributes,
+  DetailedHTMLProps,
+  ImgHTMLAttributes,
+} from 'react';
 import type { CSSProp } from 'styled-components';
 
-export type StyledImageProps =  DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & {
+export type StyledImageProps = DetailedHTMLProps<
+  ImgHTMLAttributes<HTMLImageElement>,
+  HTMLImageElement
+> & {
   css?: CSSProp;
   tw?: string;
 };
 
-export type StyledLinkProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
+export type StyledLinkProps = DetailedHTMLProps<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+> & {
   css?: CSSProp;
   tw?: string;
 };
